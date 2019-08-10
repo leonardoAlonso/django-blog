3ee5da90b48079e1b4a570b21bfedcdedac913de
Leonardo <leonardoalonsososa@gmail.com>
2019-08-10 17:22:48 -0500

    Mostrar numero de articulos por categoria

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
 # Create your views here.
-from .models import Articulo
+from .models import Articulo, Categoria, Tag
+from django.db.models import Q
 
 
 class HomeView(TemplateView):
@@ -17,5 +18,29 @@ class HomeView(TemplateView):
 class BlogView(ListView):
     template_name = 'blog.html'
     context_object_name = 'articulos'
-    queryset = Articulo.objects.filter(status=1)
     paginate_by = 4
+
+    def get_queryset(self):
+        queryset = Articulo.objects.filter(status=1)
+        parameter = self.request.GET.get('search')
+        if parameter:
+            print(parameter)
+            queryset = Articulo.objects.filter(Q(
+                categorias__nombre__contains=parameter) | Q(tags__nombre__contains=parameter)).distinct()
+            print(queryset)
+        return queryset
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(BlogView, self).get_context_data(**kwargs)
+        context['latest'] = Articulo.objects.all()[:3]
+        categorias = Categoria.objects.all()
+        categorias_dic = {}
+        for categoria in categorias:
+            id = categoria.id
+            if Articulo.objects.filter(categorias__id=id):
+                categorias_dic[categoria.nombre] = Articulo.objects.filter(
+                    categorias__id=id).count()
+        context['categorias'] = categorias_dic
+        print(context['categorias'])
+        context['tags'] = Tag.objects.all()
+        return context
